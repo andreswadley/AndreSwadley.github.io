e9176d2413b6ba50995a399b674782fb46717996
Andre Swadley <>
2021-05-12 23:41:50 +0000

    1.0

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 echo Please Wait....
-cp -r gemBINS ../.gem
+mkdir ../.gem
+cp -r gemBINS/* ../.gem
 export PATH=/home/runner/.gem/ruby/2.5.0/bin:$PATH
 jekyll -v
 cd site
-jekyll serve -P 8080 --host=0.0.0.0
+bundle exec jekyll serve -P 8080 --host=0.0.0.0
